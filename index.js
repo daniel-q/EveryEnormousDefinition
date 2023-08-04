a61364b4445ec716b56b485a1ee57d8863456bfa
daniel-q <daniel.q.aquino@gmail.com>
2023-08-04 21:57:49 +0000

    mudei um pouco

---
+++ index.js
@@ -0,0 +1,17 @@
+while(true){
+  nome = prompt("Digite seu nome")
+  nascimento = parseInt(prompt("Digite o ano de nascimento (entre 1922 e 2022)"))
+  if(nome=='' || /\d/.test(nome)){
+    console.log("Nome invalido")
+  }
+  else if(nascimento >=1922 && nascimento <=2021){
+    console.log(nome+" completou "+(2022-nascimento)+ " anos em 2022")
+    break
+  }
+  else if(isNaN(nascimento)){
+    console.log("O ano de nascimento tem que ser um numero inteiro!")
+  }
+  else{
+    console.log("Ano de nascimento invalido Invalida")
+  }
+}
